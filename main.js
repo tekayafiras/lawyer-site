65e9f5083dcb6ce80c1bfa1faea259e492996914
tekayafiras <63792357+tekayafiras@users.noreply.github.com>
2021-02-18 15:42:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nav = document.querySelector('.carousel-nav');
+const navbutton = Array.from(nav.children);
+
+const slidesize = slides[0].getBoundingClientRect().width; //  nchoufou l width w height w hajet okhra men screen ma7toutin fo object
+
+// bech n7ottou slides next to each other
+
+slides[0].style.left = 0;
+slides[1].style.left = slidesize*1 +'px';
+slides[2].style.left = slidesize*2 +'px';
+
+const movetoslide = (track, currentslide, targetslide)=>{
+    track.style.transform = 'translateX(-'+targetslide.style.left;
+    currentslide.classList.remove('current');
+    targetslide.classList.add('current');
+}
+
+
+
+
+nav.addEventListener('click', e =>{
+    //console.log(e.target.closest('button'));
+    const targetbutton = e.target.closest('button');
+
+    if(!targetbutton) return;
+
+    const currentslide = track.querySelector('.current');
+    const currentbutton = nav.querySelector('.current');
+    const targetindex = navbutton.findIndex(navbutton => navbutton === targetbutton);
+    const targetslide = slides[targetindex];
+    movetoslide(track, currentslide, targetslide);
+
+    currentbutton.classList.remove('current');
+    targetbutton.classList.add('current');
+});
